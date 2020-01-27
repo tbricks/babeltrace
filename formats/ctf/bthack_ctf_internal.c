28e2a068c8c138a16104da745fefaff863ad50ad
Roman Sayahov <Roman.Sayahov@itiviti.com>
2020-01-27 18:19:29 +0300

    TB-100312: Use opaque pointer to persistent babeltrace fields declaration
    Co-authored-by: Anton Smyk <Anton.Smyk@itiviti.com>

---
+++ formats/ctf/bthack_ctf_internal.c
@@ -0,0 +1,60 @@
+/*
+ * BabelTrace Hooks into CTF format
+ *
+ * Copyright 2020 Itiviti Group AB, EfficiOS Inc. and Linux Foundation
+ *
+ * Author: Roman Sayahov <Roman.Sayahov@itiviti.com>
+ * Author: Anton Smyk <Anton.Smyk@itiviti.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <babeltrace/bthack_ctf.h>
+
+#include <babeltrace/ctf-ir/metadata.h>
+#include <babeltrace/ctf/events-internal.h>
+
+#include <assert.h>
+
+
+const struct bthack_ctf_persistent_declaration *
+bthack_ctf_fields_decl_from_event(const struct bt_ctf_event * event)
+{
+	assert(NULL != event);
+	assert(NULL != event->parent);
+	assert(NULL != event->parent->event_fields);
+	assert(NULL != event->parent->event_fields->declaration);
+
+	const struct declaration_struct *d =
+			event->parent->event_fields->declaration;
+	return (const struct bthack_ctf_persistent_declaration *) d;
+}
+
+
+const struct bthack_ctf_persistent_declaration *
+bthack_ctf_fields_decl_from_event_decl(const struct bt_ctf_event_decl * decl)
+{
+	assert(NULL != decl);
+	assert(NULL != decl->parent.fields_decl);
+
+	const struct declaration_struct *d =
+			decl->parent.fields_decl;
+	return (const struct bthack_ctf_persistent_declaration *) d;
+}
---
+++ include/babeltrace/bthack_ctf.h
@@ -0,0 +1,56 @@
+#ifndef BABELTRACE_BTHACK_CTF_H
+#define BABELTRACE_BTHACK_CTF_H
+
+/*
+ * BabelTrace Hooks into CTF format
+ *
+ * Copyright 2020 Itiviti Group AB, EfficiOS Inc. and Linux Foundation
+ *
+ * Author: Roman Sayahov <Roman.Sayahov@itiviti.com>
+ * Author: Anton Smyk <Anton.Smyk@itiviti.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/* Babeltrace structs opaque to the user */
+struct bt_ctf_event;
+struct bt_ctf_event_decl;
+
+
+/* Itiviti inventions */
+struct bthack_ctf_persistent_declaration;
+
+const struct bthack_ctf_persistent_declaration *
+bthack_ctf_fields_decl_from_event(const struct bt_ctf_event *);
+
+const struct bthack_ctf_persistent_declaration *
+bthack_ctf_fields_decl_from_event_decl(const struct bt_ctf_event_decl *);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BABELTRACE_BTHACK_CTF_H */
